26133f00aa069bf4025999bb10cb7df5205b0f90
DuongQuyen1309 <minhquyen130904@gmail.com>
2025-07-02 11:30:10 +0700

    add fee eth for contract owner

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -5,9 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SwapToken {
     address payable contractOwner;
-    uint256 fee;
     mapping (address => mapping(address => uint256)) public tokenPair;
-    mapping (address => mapping(address => bool)) public tokenPairExists;
+    // mapping (address => mapping(address => bool)) public tokenPairExists;
 
     event TokenSwapEvent(address _token1, address _token2, uint256 _amountFrom, uint256 _amountTo);  
     event ChangeRateEvent(address _token1, address _token2, uint256 _rate);
@@ -15,7 +14,6 @@ contract SwapToken {
     event AddTokenIntoContract(address _token1, address _token2, uint256 _rate);
     constructor() {
         contractOwner = payable(msg.sender);
-        fee = 30;
     }
 
     modifier onlyOwner {
@@ -24,47 +22,40 @@ contract SwapToken {
     }
 
     function addToken(address _token1, address _token2, uint256 _rate) public onlyOwner {
-        require(!tokenPairExists[_token1][_token2], "Token pair already exists");
+        require(tokenPair[_token1][_token2] == 0, "Token pair already exists");
         tokenPair[_token1][_token2] = _rate;
-        tokenPairExists[_token1][_token2] = true;
         emit AddTokenIntoContract(_token1, _token2, _rate);
     }
     function setRate(address _token1, address _token2, uint256 rate) public onlyOwner {
         tokenPair[_token1][_token2] = rate;
         emit ChangeRateEvent(_token1, _token2, rate);
     }
-
-    function setFee(uint256 _fee) public onlyOwner {
-        fee = _fee;
-        emit ChangeFeeEvent(address(this), fee);
-    }
     
     function getRate(address _token1, address _token2) public view returns (uint256) {
         return tokenPair[_token1][_token2];
     }
 
-    function getFee() public view returns (uint256) {
-        return fee;
-    }
-
-    function swapToken(address _fromToken, address _toToken, uint256 _amount) public {
+    function swapToken(address _fromToken, address _toToken, uint256 _amount) public payable {
+        uint256 fee = 0.001 ether; 
+        require (msg.value > 0, "Can not swap same token");
+        require (tokenPair[_fromToken][_toToken] != 0 || tokenPair[_toToken][_fromToken] != 0, "Not exist");
         require (_amount > 0, "Amount must be greater than 0");
         ERC20 fromToken = ERC20(_fromToken);
         ERC20 toToken = ERC20(_toToken);
-        require (fromToken.balanceOf(msg.sender) >= _amount, "Account can not have enough balance to swap");
-        uint256 receivedTokenNotFee = _amount * tokenPair[_fromToken][_toToken];
-        uint256 swapFee = (receivedTokenNotFee * fee)/100;
-        uint256 actualReceivedToken = receivedTokenNotFee - swapFee;
-        //TODO NOTICE
-        bool isSuccess = fromToken.approve(address(this), _amount);
-        isSuccess = fromToken.transferFrom(msg.sender, address(this), _amount );
-        isSuccess = toToken.approve(address(this), actualReceivedToken);
-        isSuccess = toToken.transferFrom(address(this), msg.sender, actualReceivedToken);
-        emit TokenSwapEvent(_fromToken, _toToken, _amount, actualReceivedToken);
+        require (fromToken.balanceOf(msg.sender) >= _amount * (10 ** uint256(fromToken.decimals())), "Account can not have enough balance to swap");
+        uint256 receivedToken;
+        if (tokenPair[_fromToken][_toToken] != 0) {
+            receivedToken = _amount * (10 ** uint256(fromToken.decimals())) * tokenPair[_fromToken][_toToken];
+        }
+        if (tokenPair[_toToken][_fromToken] != 0) {
+            receivedToken = _amount * (10 ** uint256(fromToken.decimals())) / tokenPair[_toToken][_fromToken];
+        }
+        //TODO NOTICE approve
+        bool isSuccess = fromToken.approve(address(this), _amount * (10 ** uint256(fromToken.decimals())));
+        isSuccess = fromToken.transferFrom(msg.sender, address(this), _amount * (10 ** uint256(fromToken.decimals())));
+        isSuccess = toToken.approve(address(this), receivedToken);
+        isSuccess = toToken.transferFrom(address(this), msg.sender, receivedToken);
+        contractOwner.transfer(fee);
+        emit TokenSwapEvent(_fromToken, _toToken, _amount, receivedToken);
     }
-    
-    // function buyToken1(address _token, uint256 _amount) public payable onlyOwner {
-    //     MyToken1 token = MyToken1(_token);
-    //     token.buyToken{value: msg.value}(_amount);
-    // }
 }
