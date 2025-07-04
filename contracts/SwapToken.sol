edac2b82b24fa64abfd46edacfdfa659c47d663b
DuongQuyen1309 <minhquyen130904@gmail.com>
2025-07-04 15:27:00 +0700

    optimize contract

--- contracts/SwapToken.sol
+++ contracts/SwapToken.sol
@@ -5,15 +5,16 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SwapToken {
     address payable contractOwner;
+    uint256 public fee ;
     mapping (address => mapping(address => uint256)) public RateOfTokenPair;
-
-    event TokenSwapEvent(address _token1, address _token2, uint256 _amountFrom, uint256 _amountTo);  
-    event ChangeRateEvent(address _token1, address _token2, uint256 _rate);
+    event TokenSwapEvent(address _fromToken, address _toToken, uint256 _fromAmount, uint256 _toAmount);  
+    event ChangeRateEvent(address _fromToken, address _toToken, uint256 _rate);
     event ChangeFeeEvent(address _contractAddress, uint256 _fee);
-    event AddTokenIntoContract(address _token1, address _token2, uint256 _rate);
+    event AddTokenIntoContract(address _fromToken, address _toToken, uint256 _rate);
 
     constructor() {
         contractOwner = payable(msg.sender);
+        fee = 0.001 ether;
     }
 
     modifier onlyOwner {
@@ -21,79 +22,78 @@ contract SwapToken {
         _;
     }
 
-    function addToken(address _token1, address _token2, uint256 _rate) public onlyOwner {
-        require(RateOfTokenPair[_token1][_token2] == 0, "Token pair already exists");
-        RateOfTokenPair[_token1][_token2] = _rate;
-        emit AddTokenIntoContract(_token1, _token2, _rate);
+    function addToken(address _fromToken, address _toToken, uint256 _rate) public onlyOwner {
+        require(_rate > 0, "Rate must be greater than 0");
+        require(_fromToken != _toToken, "fromToken must be different from toToken");
+        require(RateOfTokenPair[_fromToken][_toToken] == 0, "Token pair already exists");
+        RateOfTokenPair[_fromToken][_toToken] = _rate;
+        emit AddTokenIntoContract(_fromToken, _toToken, _rate);
     }
-    function setRate(address _token1, address _token2, uint256 rate) public onlyOwner {
-        RateOfTokenPair[_token1][_token2] = rate;
-        emit ChangeRateEvent(_token1, _token2, rate);
+    function setRate(address _fromToken, address _toToken, uint256 rate) public onlyOwner {
+        RateOfTokenPair[_fromToken][_toToken] = rate;
+        emit ChangeRateEvent(_fromToken, _toToken, rate);
     }
     
-    function getRate(address _token1, address _token2) public view returns (uint256) {
-        return RateOfTokenPair[_token1][_token2];
+    function getRate(address _fromToken, address _toToken) public view returns (uint256) {
+        return RateOfTokenPair[_fromToken][_toToken];
     }
 
-    function swapTokenWithETH(address _fromToken, address _toToken, uint256 _amount) public payable{
-        uint256 fee = 0.001 ether; 
-        uint256 receivedTokenAmount;
+    function setFee(uint256 _fee) public onlyOwner {
+        fee = _fee;
+        emit ChangeFeeEvent(address(this), _fee);
+    }
+
+    function swapToken(address _fromToken, address _toToken, uint256 _amount) public payable {
         require(_amount > 0, "Amount must be greater than 0");
         require(RateOfTokenPair[_fromToken][_toToken] != 0 || RateOfTokenPair[_toToken][_fromToken] != 0, "Token pair does not exist in contract");
-        if (_fromToken == 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE) {
-            require (msg.value ==  fee + _amount, "Need to pay amount fee to swap token");
-            ERC20 tokenTo = ERC20(_toToken);
-            if (RateOfTokenPair[_fromToken][_toToken] != 0) {
-                receivedTokenAmount = _amount * RateOfTokenPair[_fromToken][_toToken] / (10 ** uint256(18)) * (10 ** uint256(tokenTo.decimals()));
-            }
-            if (RateOfTokenPair[_toToken][_fromToken] != 0) {
-                receivedTokenAmount = _amount / RateOfTokenPair[_toToken][_fromToken] / (10 ** uint256(18)) * (10 ** uint256(tokenTo.decimals()));
-            }
-            tokenTo.transfer(msg.sender, receivedTokenAmount);
-            contractOwner.transfer(fee);
-            emit TokenSwapEvent(_fromToken, _toToken, _amount, receivedTokenAmount);
-            return;
-        } 
-        require (msg.value == fee, "Need to pay amount fee to swap token");
-        ERC20 tokenFrom = ERC20(_fromToken);
-        if (RateOfTokenPair[_fromToken][_toToken] != 0) {
-            receivedTokenAmount = _amount * RateOfTokenPair[_fromToken][_toToken] / (10 ** uint256(tokenFrom.decimals())) * (10 ** uint256(18));
-        }
-        if (RateOfTokenPair[_toToken][_fromToken] != 0) {
-            receivedTokenAmount = _amount / RateOfTokenPair[_toToken][_fromToken] / (10 ** uint256(tokenFrom.decimals())) * (10 ** uint256(18));
-        }
-        bool isSuccess = tokenFrom.transferFrom(msg.sender, address(this), _amount);
-        require(isSuccess, "Transfer fromToken to contract failed");
-        payable(msg.sender).transfer(receivedTokenAmount);
-        contractOwner.transfer(fee);
-        emit TokenSwapEvent(_fromToken, _toToken, _amount, receivedTokenAmount);
-        return;
+
+        //transfer fromToken to contract
+        HandleAmountFrom(_fromToken, _amount);
+
+        //calculate received amount of toToken
+        uint256 amountTo = calcAmountTo(_fromToken, _toToken, _amount);
+
+        //transfer received amount to msg.sender 
+        HandleAmountTo(_toToken, amountTo);
+        
+        emit TokenSwapEvent(_fromToken, _toToken, _amount, amountTo);
     }
 
-    // need to consider transferFrom
-    function swapToken(address _fromToken, address _toToken, uint256 _amount) public payable returns (uint256) {
-        uint256 fee = 0.001 ether; 
-        require (msg.value == fee, "Need to pay amount fee to swap token");
-        require (RateOfTokenPair[_fromToken][_toToken] != 0 || RateOfTokenPair[_toToken][_fromToken] != 0, "Token pair does not exist in contract");
-        require (_amount > 0, "Amount must be greater than 0");
-        ERC20 fromToken = ERC20(_fromToken);
-        ERC20 toToken = ERC20(_toToken);
-        require (fromToken.balanceOf(msg.sender) >= _amount, "Account can not have enough balance to swap");
-        uint256 receivedTokenAmount;
-        if (RateOfTokenPair[_fromToken][_toToken] != 0) {
-            receivedTokenAmount = _amount * RateOfTokenPair[_fromToken][_toToken] / (10 ** uint256(fromToken.decimals())) * (10 ** uint256(toToken.decimals()));
+    function HandleAmountFrom(address _fromToken, uint256 _amount) internal {
+        if(_fromToken == address(0)) {
+            require(msg.value == fee + _amount, "Need to pay amount fee to swap token");
+            return;
         }
-        if (RateOfTokenPair[_toToken][_fromToken] != 0) {
-            receivedTokenAmount = _amount * (10 ** uint256(toToken.decimals())) / RateOfTokenPair[_toToken][_fromToken] / (10 ** uint256(fromToken.decimals())) ;
-        }
-        bool isSuccess;
-        isSuccess = fromToken.transferFrom(msg.sender, address(this), _amount);
-        require(isSuccess, "Transfer fromToken to contract failed");
-        isSuccess = toToken.transfer(msg.sender, receivedTokenAmount);
-        require(isSuccess, "Transfer contract to msg.sender failed");
-        contractOwner.transfer(fee);
-        emit TokenSwapEvent(_fromToken, _toToken, _amount, receivedTokenAmount);
-        return receivedTokenAmount;
+        require(msg.value == fee, "Need to pay amount fee to swap token");
+        require(ERC20(_fromToken).allowance(msg.sender, address(this)) >= _amount, "Account can not have allowance fromToken to contract");
+        require(ERC20(_fromToken).transferFrom(msg.sender, address(this), _amount),"Transfer fromToken to contract failed");
     }
+
+    function GetDecimalsOfTokens(address _fromToken, address _toToken) public view returns (uint256, uint256) {
+        uint256 fromTokenDecimal = (_fromToken == address(0)) ? 18 : ERC20(_fromToken).decimals();
+        uint256 toTokenDecimal = (_toToken == address(0)) ? 18 : ERC20(_toToken).decimals();
+        return (fromTokenDecimal, toTokenDecimal);
+    }
+    
+    function HandleAmountTo(address _toToken, uint256 _amount) internal {
+        if (_toToken == address(0)) {
+            require(address(this).balance >= _amount, "Contract can not have enough ETH to swap");
+            (bool sent,) = msg.sender.call{value: _amount}("");
+            require(sent, "Error sending native token");
+            return;
+        } 
+        require(ERC20(_toToken).balanceOf(address(this)) >= _amount, "Account can not have enough toToken to swap");
+        require(ERC20(_toToken).transfer(msg.sender, _amount),"Transfer contract to msg.sender failed");
+    }
+
+    function calcAmountTo(address _fromToken, address _toToken, uint256 _amount) public view returns (uint256){
+        (uint256 fromTokenDecimal, uint256 toTokenDecimal) = GetDecimalsOfTokens(_fromToken, _toToken);
+
+        if (RateOfTokenPair[_fromToken][_toToken] != 0) {
+            return _amount * RateOfTokenPair[_fromToken][_toToken] * (10 ** toTokenDecimal) / (10 ** fromTokenDecimal) ;
+        }
+        return _amount * (10 ** toTokenDecimal) / RateOfTokenPair[_toToken][_fromToken] / (10 ** fromTokenDecimal) ;
+    }
+    
     receive() external payable {}
 }
--- test/SwapToken.js
+++ test/SwapToken.js
@@ -3,77 +3,82 @@ const { ethers } = require("hardhat");
 
 describe("SwapToken", function () {
   let swapToken, myToken1, myToken2, owner, addr1;
+  const nativeToken = "0x0000000000000000000000000000000000000000";
 
   beforeEach(async function () {
-    console.log("before each...");
     [owner, addr1] = await ethers.getSigners();
 
     const MyToken1 = await ethers.getContractFactory("MyToken1");
     myToken1 = await MyToken1.deploy(100000,100000);
-    console.log("MyToken1 deployed");
 
     const MyToken2 = await ethers.getContractFactory("MyToken2");
     myToken2 = await MyToken2.deploy(100000,100000);
-    console.log("MyToken2 deployed");
 
     const SwapToken = await ethers.getContractFactory("SwapToken");
     swapToken = await SwapToken.deploy();
-    console.log("SwapToken deployed");
 
-    //TODO consider parseUnit and parseEther to synchronize
     await myToken1.transferOut(addr1.address, ethers.parseUnits("50", 18));
     await myToken2.transferOut(addr1.address, ethers.parseUnits("50", 18));
+
     await owner.sendTransaction({
       to: swapToken.target, 
       value: ethers.parseEther("50")
     });
-    console.log("balanceOfAddress1tk1:", await myToken1.balanceOf(addr1.address));
-    console.log("balanceOfAddress1tk2:", await myToken2.balanceOf(addr1.address));
+
     await myToken1.transferOut(swapToken.target, ethers.parseUnits("50", 18));
     await myToken2.transferOut(swapToken.target, ethers.parseUnits("50", 18));
-    console.log("tk1",myToken1.target);
-    console.log("tk2",myToken2.target);
-    console.log("swapToken",swapToken.target);
     await swapToken.addToken(myToken1.target, myToken2.target, 3);
-    await swapToken.addToken("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", myToken2.target, 1);
-    console.log("transfer tokens");
+    await swapToken.addToken(nativeToken, myToken2.target, 1);
   });
 
-  it("should swap tokens", async function () {
+  it("Success: swap tokens without ETH", async function () {
     await myToken1.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
     await myToken2.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
-    console.log("balanceOfMyToken1before", await myToken1.balanceOf(swapToken.target));
-    console.log("balanceOfMyToken2before", await myToken2.balanceOf(swapToken.target));
-    const ethBalanceAddr1 = await ethers.provider.getBalance(addr1.address);
-    console.log("ETH balance of addr1 before:", ethBalanceAddr1.toString());
-    const ethBalanceBefore = await ethers.provider.getBalance(swapToken.target);
-    console.log("ETH balance of SwapToken contract before:", ethBalanceBefore.toString());
-    const returnedValue = await swapToken.connect(addr1).swapToken(myToken2.target,myToken1.target, ethers.parseUnits("10", 18), {value: ethers.parseEther("0.001")});
-    const ethBalance = await ethers.provider.getBalance(swapToken.target);
-    console.log("ETH balance of SwapToken contract after:", ethBalance.toString());
-    const ethBalanceAddr1After = await ethers.provider.getBalance(addr1.address);
-    const receipt = await returnedValue.wait();
-    const event = receipt.logs.map(log => {
-      try {
-        return swapToken.interface.parseLog(log);
-      } catch {
-        return null;
-      }
-    })
-    .find(e => e && e.name === "TokenSwapEvent");
-    if (event) {
-      const receivedTokenAmount = event.args._amountTo;
-      console.log("receivedTokenAmount from event:", receivedTokenAmount.toString());
-    } else {
-      console.log("TokenSwapEvent not found in logs");
-    }
-    console.log("ETH balance of addr1 after:", ethBalanceAddr1After.toString());
-    console.log("balanceOfMyToken1after", await myToken1.balanceOf(swapToken.target));
-    console.log("balanceOfMyToken2after", await myToken2.balanceOf(swapToken.target));
+
+    await swapToken.connect(addr1).swapToken(myToken2.target,myToken1.target, ethers.parseUnits("10", 18), {value: ethers.parseEther("0.001")});
+    
     const balance1 = await myToken1.balanceOf(swapToken.target);
     const balance2 = await myToken2.balanceOf(swapToken.target);
-
-    expect(balance1).to.equal(60000000000000000000n); 
-    expect(balance2).to.equal(36000000000000000000n);
+    expect(balance1).to.equal(46666666666666666667n); 
+    expect(balance2).to.equal(60000000000000000000n);
   });
+  
+  it("Success:swap ERC token to receive ETH", async function () {
+    await myToken1.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
+    await myToken2.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
+
+    await swapToken.connect(addr1).swapToken(myToken2.target, nativeToken, ethers.parseUnits("10", 18), {value: ethers.parseEther("0.001")});
+
+    const balance1 = await myToken1.balanceOf(swapToken.target);
+    const balance2 = await myToken2.balanceOf(swapToken.target);
+    expect(balance1).to.equal(50000000000000000000n); 
+    expect(balance2).to.equal(60000000000000000000n);
+  });
+
+  it("Success: swap ETH to receive ERC20 ", async function () {
+    await myToken1.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
+    await myToken2.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
+    
+    await swapToken.connect(addr1).swapToken(nativeToken,myToken2.target, ethers.parseUnits("10", 18), {value: ethers.parseEther("10.001")});
+    
+    const balance1 = await myToken1.balanceOf(swapToken.target);
+    const balance2 = await myToken2.balanceOf(swapToken.target);
+    expect(balance1).to.equal(50000000000000000000n); 
+    expect(balance2).to.equal(40000000000000000000n);
+  });
+
+  // it("Fail: Swapping ETH with ERC that have amount = 0", async function () {
+  //   await myToken1.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
+  //   await myToken2.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
+  //   await expect(
+  //   swapToken.connect(addr1).swapToken(myToken2.target, "0x0000000000000000000000000000000000000000", ethers.parseUnits("0", 18), { value: ethers.parseEther("0.001") })
+  //   ).to.be.revertedWith("Amount must be greater than 0");
+  // });
+  // it("Fail: Swapping ETH with ERC that have rate = 0", async function () {
+  //   await myToken1.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
+  //   await myToken2.connect(addr1).approve(swapToken.target, ethers.parseUnits("50", 18));
+  //   await expect(
+  //   swapToken.connect(addr1).swapToken("0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE", "0x0000000000000000000000000000000000000000", ethers.parseUnits("0", 18), { value: ethers.parseEther("0.001") })
+  //   ).to.be.revertedWith("Token pair does not exist in contract");
+  // });
 });
