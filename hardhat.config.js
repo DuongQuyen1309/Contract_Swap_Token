845dd0af0014135c2b4d53bd716fee73696ed567
DuongQuyen1309 <minhquyen130904@gmail.com>
2025-07-07 09:21:15 +0700

    add file for config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("@nomicfoundation/hardhat-ignition-ethers");
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+      sepolia: {
+        url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`, // hoặc Alchemy URL
+        accounts: [process.env.PRIVATE_KEY] // ví Sepolia của bạn
+      }
+    }
 };
