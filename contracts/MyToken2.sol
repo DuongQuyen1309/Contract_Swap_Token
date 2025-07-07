739a17c05ce030a157932f0bbad1cc9f422ee336
DuongQuyen1309 <minhquyen130904@gmail.com>
2025-07-07 15:18:18 +0700

    remove duplication of file sol

--- contracts/MyToken2.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-contract MyToken2 is ERC20 { 
-    constructor(string memory _name, string memory _symbol, uint256 _initialSupply) ERC20(_name, _symbol) {
-        _mint(msg.sender, _initialSupply * (10 ** uint256(decimals())));
-    }
-}
